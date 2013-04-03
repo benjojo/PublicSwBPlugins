2d1e93918a445325ca5b1cd2898dd3d32cb95003
Ben Cox <Ben@Benjojo.co.uk>
2013-04-03 19:08:28 +0100

    The starts of Bucket

---
+++ Bucket/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace discord.plugins
+{
+    public class Bucket : IPlugin
+    {
+        public Bucket() { }
+
+        public String Name { get { return "Bucket"; } }
+        public String Desc { get { return "Factoid Engine"; } }
+        public String Auth { get { return "Benjojo & Rohan"; } }
+
+
+        public void Load()
+        {
+            discord.core.Events.OnChatMsgCallback += new core.ChatMsgEventHandler(Events_OnChatMsgCallbaack);
+            Console.WriteLine("Bukkit Loaded.");
+        }
+
+        public void Unload()
+        {
+            discord.core.Events.OnChatMsgCallback -= new core.ChatMsgEventHandler(Events_OnChatMsgCallbaack);
+
+        }
+
+        void Events_OnChatMsgCallbaack(SteamKit2.SteamFriends.ChatMsgCallback msg)
+        {
+
+        }
+        
+    }
+}
