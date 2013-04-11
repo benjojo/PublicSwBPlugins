baa4beb91327ed1dbc609bd01ced92242a7f7215
Rohan Singh <rohan-singh@hotmail.com>
2013-04-11 12:41:29 -0400

    Forgot Objects.cs

--- Bucket/Objects.cs
+++ Bucket/Objects.cs
@@ -8,16 +8,20 @@ namespace discord.plugins
     public class FactRow
     {
         public readonly uint Id;
+        public readonly ulong SteamId;
         public readonly string Fact;
         public readonly string Tidbit;
         public readonly string Verb;
+        public readonly bool Protected;
 
         public FactRow(dynamic row)
         {
             Id = row.id;
+            SteamId = (ulong)row.SteamID;
             Fact = row.fact;
             Tidbit = row.tidbit;
             Verb = row.verb;
+            Protected = row.@protected;
         }
     }
 }
